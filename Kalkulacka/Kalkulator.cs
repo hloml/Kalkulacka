6535c853e140fb5a8573c91cb864e3da358fc2a4
jaroslavnemec <jaranem@students.zcu.cz>
2015-04-12 00:21:36 +0200

    repaired last merge, commented my methods and some little fixes

--- Kalkulacka/Kalkulator.cs
+++ Kalkulacka/Kalkulator.cs
@@ -34,7 +34,7 @@ namespace Kalkulacka
             while (true)
             {
                 InsertValues();
-                String countedTariff = CountTariff(startDateG, endDateG, "ZTP");
+                String countedTariff = CountTariff(startDateG, endDateG, discount);
 
 
             }
@@ -43,174 +43,251 @@ namespace Kalkulacka
 
 
         // Load tarifs for all zones from excel file and save them into dictionary
+
         public static Dictionary<String, List<Tarif>> LoadExcel(string filename)
         {
+
             Application xlsApp = new Application();
 
             if (!System.IO.File.Exists(filename))
             {
+
                 Console.WriteLine("File doesnt exist");
+
                 return null;
+
             }
 
             if (xlsApp == null)
             {
+
                 Console.WriteLine("EXCEL could not be started. Check that your office installation and project references are correct.");
+
                 return null;
+
             }
 
             Workbook wb = xlsApp.Workbooks.Open(filename,
+
                 0, true, 5, "", "", true, XlPlatform.xlWindows, "\t", false, false, 0, true);
+
             Sheets sheets = wb.Worksheets;
 
-            Dictionary<String, List<Tarif>> tarifs_dictionary = new Dictionary<string,List<Tarif>>();
+            Dictionary<String, List<Tarif>> tarifs_dictionary = new Dictionary<string, List<Tarif>>();
 
-            for (int list_index = 1; list_index <= sheets.Count; list_index++) {    // iterate over all lists from excel
+            for (int list_index = 1; list_index <= sheets.Count; list_index++)
+            {    // iterate over all lists from excel
 
-               Worksheet ws = (Worksheet)sheets.get_Item(list_index);
-       
-               int  columns_count = ws.UsedRange.Columns.Count;
-               string[] category = new string[columns_count];
-               float[,] days_tarif = new float[columns_count, NUMBER_OF_DAYS + 1];      // for saving values for 1..123 days
+                Worksheet ws = (Worksheet)sheets.get_Item(list_index);
 
-            Worksheet ws = (Worksheet)sheets.get_Item(list_index);
-            
-            
-            
-            bool isNumeric, isCategory;
-            float column_value;
-            int column_index, columns_count = ws.UsedRange.Columns.Count, first_column;
-            string first_column_string;
-            string[] category = new string[columns_count];
-            float[,] days_tarif = new float[columns_count, NUMBER_OF_DAYS + 1];
 
-               Dictionary<String, String>[] dictionary = new Dictionary<string, string>[columns_count];     // for other values, storing them with key (380 dni, 180dni, mesicni ..)
 
-               for (int j = 0; j < columns_count; j++)      // initiate array of dictionaries
-               {
-                   dictionary[j] = new Dictionary<string, string>();
-               }
+                int columns_count = ws.UsedRange.Columns.Count;
 
-               IterateOverAllCells(ws, dictionary, category, days_tarif);       // iterate all cells in actual list
-      
-               List<Tarif> tarifs = SaveTarifsToList(category, days_tarif, dictionary);    // create object tarif for each category and save objects to arraylist
-               tarifs_dictionary.Add(ws.Name, tarifs);      // save tarifs to dictionary with zone name as a key
-            
+                string[] category = new string[columns_count];
+
+                float[,] days_tarif = new float[columns_count, NUMBER_OF_DAYS + 1];      // for saving values for 1..123 days
+
+                Dictionary<String, String>[] dictionary = new Dictionary<string, string>[columns_count];     // for other values, storing them with key (380 dni, 180dni, mesicni ..)
+
+                for (int j = 0; j < columns_count; j++)      // initiate array of dictionaries
+                {
+
+                    dictionary[j] = new Dictionary<string, string>();
+
+                }
+
+                IterateOverAllCells(ws, dictionary, category, days_tarif);       // iterate all cells in actual list
+
+
+
+                List<Tarif> tarifs = SaveTarifsToList(category, days_tarif, dictionary);    // create object tarif for each category and save objects to arraylist
+
+                tarifs_dictionary.Add(ws.Name, tarifs);      // save tarifs to dictionary with zone name as a key
+
+
+
             }
+
             return tarifs_dictionary;
+
         }
 
-
-
         // iterate over all cells in excel and find all categories. For categories fill array (for 1..123 days) and dictionary obtained from parameters
-        private static void IterateOverAllCells(Worksheet ws, Dictionary<String, String>[] dictionary, string[] category, float[,] days_tarif  )
+
+        private static void IterateOverAllCells(Worksheet ws, Dictionary<String, String>[] dictionary, string[] category, float[,] days_tarif)
         {
+
             bool isNumeric, isCategory;
+
             float column_value;
+
             int column_index, columns_count = ws.UsedRange.Columns.Count, first_column;
+
             string first_column_string;
-            
-            
+
+
+
+
+
             foreach (Range row in ws.UsedRange.Rows)        // iterate all rows
             {
+
                 column_index = 0;
+
                 first_column = -1;
+
                 isCategory = false;
+
                 first_column_string = "";
 
                 foreach (Range cell in row.Columns)     // iterate all columns
                 {
+
                     column_index++;
 
                     if (cell.Value2 != null)    // cell is not empty
                     {
+
                         isNumeric = float.TryParse(cell.Value2.ToString(), out column_value);
 
                         if (isNumeric)  // cell value is number
                         {
+
                             if (column_index == 1)     // first column
                             {
+
                                 first_column = (int)Math.Ceiling(column_value); ;
+
                             }
+
                             else if (first_column != -1)    // first column is number (1, 123)
                             {
+
                                 if (first_column > 0 && first_column <= NUMBER_OF_DAYS)
                                 {
+
                                     days_tarif[column_index, first_column] = column_value;
+
                                 }
-                                else   
+
+                                else
                                 {
+
                                     dictionary[column_index].Add(first_column.ToString(), cell.Value2.ToString());
-                                }
-                            }
-                            else if (!string.IsNullOrEmpty(first_column_string))    // first column is not number (mesicni, 380 dni)
-                            {
-                                dictionary[column_index].Add(first_column_string, cell.Value2.ToString());
-                            }
-                        }
-                        else     // cell value is string
-                        {
-                            String s = cell.Value2.ToString();
-                            if (column_index == 1)  //  first column
-                            {
-                                switch (s)
-                                {
-                                    case "dny": isCategory = true; break;
-                                    default: first_column_string = s; break;
+
                                 }
 
                             }
+
+                            else if (!string.IsNullOrEmpty(first_column_string))    // first column is not number (mesicni, 380 dni)
+                            {
+
+                                dictionary[column_index].Add(first_column_string, cell.Value2.ToString());
+
+                            }
+
+                        }
+
+                        else     // cell value is string
+                        {
+
+                            String s = cell.Value2.ToString();
+
+                            if (column_index == 1)  //  first column
+                            {
+
+                                switch (s)
+                                {
+
+                                    case "dny": isCategory = true; break;
+
+                                    default: first_column_string = s; break;
+
+                                }
+
+                            }
+
                             else if (isCategory == true)    // actual row values are category
                             {
+
                                 category[column_index] = cell.Value2.ToString();
+
                             }
+
                             else     // some other value, save to dictionary
                             {
+
                                 if (first_column != -1)
                                 {
+
                                     dictionary[column_index].Add(first_column.ToString(), s);
+
                                 }
+
                                 else if (!string.IsNullOrEmpty(first_column_string))
                                 {
+
                                     dictionary[column_index].Add(first_column_string, s);
+
                                 }
+
                             }
+
                         }
+
                     }
+
                 }
+
             }
 
         }
 
-
-
         // create object tarif for all categories and return arrayList with created objects.
+
         private static List<Tarif> SaveTarifsToList(string[] category, float[,] days_tarif, Dictionary<String, String>[] dictionary)
         {
+
             float[] tmp_array = new float[NUMBER_OF_DAYS + 1];
+
             List<Tarif> tarifs = new List<Tarif>();
+
             for (int i = 0; i < category.Length; i++)
             {
+
                 if (!string.IsNullOrEmpty(category[i]))
                 {
+
                     Tarif tarif = new Tarif();
+
                     tarif.category = category[i];
 
                     tmp_array = new float[NUMBER_OF_DAYS + 1];
+
                     for (int j = 0; j <= NUMBER_OF_DAYS; j++)
+
                         tmp_array[j] = days_tarif[i, j];
 
                     tarif.DayTarif = tmp_array;
+
                     tarif.Dictionary = dictionary[i];
+
                     tarifs.Add(tarif);
+
                 }
+
             }
-           
+
+
+
             return tarifs;
+
         }
 
 
 
+        // counts prices for day, year and half-year tariffs
         public static String CountTariff(DateTime startDate, DateTime endDate, String discount)
         {
             float price;
@@ -229,7 +306,7 @@ namespace Kalkulacka
             return null;
         }
 
-
+        // count price for day tariff
         public static float CountDaysPrice(DateTime startDate, DateTime endDate, String discount) {
             int daysDifference = DaysDifference(startDate, endDate);
 
@@ -246,7 +323,7 @@ namespace Kalkulacka
             return daysPrice;
         }
 
-
+        // count price for year tariff
         public static float Count380Price(DateTime startDate, DateTime endDate, String discount)
         {
             int daysDifference = DaysDifference(startDate, endDate);
@@ -267,7 +344,7 @@ namespace Kalkulacka
             return yearsPrice;
         }
 
-
+        // count price for half-year tariff
         public static float Count190Price(DateTime startDate, DateTime endDate, String discount)
         {
             int daysDifference = DaysDifference(startDate, endDate);
@@ -288,6 +365,7 @@ namespace Kalkulacka
             return sixMonthsPrice;
         }
 
+        // counts day difference between start and end date
         public static int DaysDifference(DateTime startDate, DateTime endDate)
         {
             int daysDifference = (int)(endDate.Date - startDate.Date).TotalDays + 1;
@@ -295,6 +373,7 @@ namespace Kalkulacka
             return daysDifference;
         }
 
+        // select right tariff by discount
         public static Tarif TariffChooser(String discount)
         {
             List<Tarif> listTariff = ListTariff(defaultZone);
@@ -313,6 +392,7 @@ namespace Kalkulacka
             return choosenTariff;
         }
 
+        // return list of all tariffs (for discounts)
         public static List<Tarif> ListTariff(String zone)
         {
             List<Tarif> listTariff;
@@ -321,7 +401,7 @@ namespace Kalkulacka
         }
 
 
-
+        // console formular for data input
         public static void InsertValues()
         {
             String sYear, sMonth, sDay;
@@ -376,6 +456,7 @@ namespace Kalkulacka
 
         }
 
+        // writte to console error mesage by error number code
         public static void ErrorWritter(float errNumberF)
         {
             int errNumber = (int)errNumberF;
