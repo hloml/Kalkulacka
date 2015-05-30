0239bd16d94c47d9bd2948bfe6be578356187804
unknown <nemec.jara@gmail.com>
2015-05-30 16:36:39 +0200

    Hiding checkboxes, repaired notes, write if its free, changed when it write results

---
+++ WebPovedCalculator/Scripts/Secret/hidecheckbox.js
@@ -0,0 +1,64 @@
+﻿
+$(function () {
+    $("#category").change(function () {
+        var str = "";
+        $("#category option:selected").each(function () {
+            str += $(this).text();
+        });
+        hideCheckbox(str);
+    })
+  .change();
+});
+
+function hideCheckbox(discount) {
+    switch (discount) {
+        case "Student (15 - 26 let)":
+            $("#isicDiscount").show();
+            $("#schoolDiscount").show();
+            $("#janskehoDiscount").hide();
+            break;
+        case "ZTP":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").hide();
+            $("#janskehoDiscount").hide();
+            break;
+        case "Firemní":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").hide();
+            $("#janskehoDiscount").hide();
+            break;
+        case "Dítě (do 6 let)":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").hide();
+            $("#janskehoDiscount").hide();
+            break;
+        case "Důchodce (do 65 let)":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").hide();
+            $("#janskehoDiscount").show();
+            break;
+        case "Důchodce (65 - 70 let)":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").hide();
+            $("#janskehoDiscount").show();
+            break;
+        case "Důchodce (70 a více let)":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").hide();
+            $("#janskehoDiscount").show();
+            break;
+        case "Dítě (6 - 15 let)":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").show();
+            $("#janskehoDiscount").hide();
+            break;
+        case "Dospělý (od 15 let)":
+            $("#isicDiscount").hide();
+            $("#schoolDiscount").hide();
+            $("#janskehoDiscount").show();
+            break;
+        default:
+            break;
+
+    }
+}
