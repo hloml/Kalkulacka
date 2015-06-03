37672ef31f823cb544871b3e2493701db8f4ebb9
jaroslavnemec <jaranem@students.zcu.cz>
2015-06-03 12:29:55 +0200

    Fixed unchecking extra checkboxes with discounts, which are not used for choosen tariff

--- WebPovedCalculator/Scripts/Secret/hidecheckbox.js
+++ WebPovedCalculator/Scripts/Secret/hidecheckbox.js
@@ -16,46 +16,66 @@ function hideCheckbox(discount) {
             $("#isicDiscount").show();
             $("#schoolDiscount").show();
             $("#janskehoDiscount").hide();
+            $("#discountsJanskeho").prop('checked', false);
             break;
         case "ZTP":
             $("#isicDiscount").hide();
             $("#schoolDiscount").hide();
             $("#janskehoDiscount").hide();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsSchool").prop('checked', false);
+            $("#discountsJanskeho").prop('checked', false);
             break;
         case "Firemní":
             $("#isicDiscount").hide();
             $("#schoolDiscount").hide();
             $("#janskehoDiscount").hide();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsSchool").prop('checked', false);
+            $("#discountsJanskeho").prop('checked', false);
             break;
         case "Dítě (do 6 let)":
             $("#isicDiscount").hide();
             $("#schoolDiscount").hide();
             $("#janskehoDiscount").hide();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsSchool").prop('checked', false);
+            $("#discountsJanskeho").prop('checked', false);
             break;
         case "Důchodce (do 65 let)":
             $("#isicDiscount").hide();
             $("#schoolDiscount").hide();
             $("#janskehoDiscount").show();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsSchool").prop('checked', false);
             break;
         case "Důchodce (65 - 70 let)":
             $("#isicDiscount").hide();
             $("#schoolDiscount").hide();
             $("#janskehoDiscount").show();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsSchool").prop('checked', false);
             break;
         case "Důchodce (70 a více let)":
             $("#isicDiscount").hide();
             $("#schoolDiscount").hide();
             $("#janskehoDiscount").show();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsSchool").prop('checked', false);
             break;
         case "Dítě (6 - 15 let)":
             $("#isicDiscount").hide();
             $("#schoolDiscount").show();
             $("#janskehoDiscount").hide();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsJanskeho").prop('checked', false);
             break;
         case "Dospělý (od 15 let)":
             $("#isicDiscount").hide();
             $("#schoolDiscount").hide();
             $("#janskehoDiscount").show();
+            $("#discountISIC").prop('checked', false);
+            $("#discountsSchool").prop('checked', false);
             break;
         default:
             break;
